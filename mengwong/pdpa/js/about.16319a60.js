852283fea43c6c316111edbc13344b4962f09b18
Wong Meng Weng <mwwong@smu.edu.sg>
2021-10-12 14:40:04 +0800

    try to bring up pdpa demo

---
+++ mengwong/pdpa/js/about.16319a60.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23");const c={class:"about"},o=Object(a["f"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(a["r"])(),Object(a["e"])("div",c,s)}const b={};b.render=u;t["default"]=b}}]);
+//# sourceMappingURL=about.16319a60.js.map
